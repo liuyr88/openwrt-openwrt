f8af7d2436d2142a863f668299492e2155931c9f
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2020-09-21 08:45:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -68,3 +68,103 @@ mv luci-app-jd-dailybonus/ package/
 #vssr-plus
 ##git clone https://github.com/liuwenwv/luci-app-vssr-plus.git package/luci-app-vssr-plus
 #git clone https://github.com/Leo-Jo-My/diy.git package/my
+#
+#
+#
+#
+#docker
+git clone https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git xiaoqingfeng/
+#mv xiaoqingfeng/docker-ce/ package/
+#mv xiaoqingfeng/luci-app-dockerman/ package/
+#端口转发
+mv xiaoqingfeng/homeredirect/ package/
+mv xiaoqingfeng/luci-app-homeredirect package/
+
+#luci-theme-infinityfreedom
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/
+
+#chinadns-ng
+#git clone https://github.com/liuyr88/luci-app-chinadns-ng.git
+#mv luci-app-chinadns-ng/ package/
+git clone https://github.com/liuyr88/feeds-xiaoqingfeng.git
+mv feeds-xiaoqingfeng/luci-app-chinadns-ng/ package/
+mv feeds-xiaoqingfeng/chinadns-ng/ package/
+#mv feeds/lienol/package/chinadns-ng/ tools/ng
+
+# Add luci-app-ssr-plus
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+popd
+
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/SuLingGG/openwrt-package
+mv package/communit/openwrt-package/package/chinadns-ng/ tools/ng
+
+# Add mentohust & luci-app-mentohust.
+git clone --depth=1 https://github.com/BoringCat/luci-app-mentohust
+git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk
+
+# Add ServerChan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+
+# Add luci-app-onliner (need luci-app-nlbwmon)
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
+
+# Add luci-app-adguardhome
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+
+# Add Rclone-OpenWrt
+git clone --depth=1 https://github.com/ElonH/Rclone-OpenWrt
+
+# Add luci-app-diskman
+git clone --depth=1 https://github.com/lisaac/luci-app-diskman
+mkdir parted
+cp luci-app-diskman/Parted.Makefile parted/Makefile
+
+# Add luci-app-dockerman
+git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman
+mkdir luci-lib-docker
+curl -s -o ./luci-lib-docker/Makefile https://raw.githubusercontent.com/lisaac/luci-lib-docker/master/Makefile
+rm -rf ../lean/luci-app-docker
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
+
+# Add tmate
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/tmate
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/msgpack-c
+
+# Add gotop
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/gotop
+
+# Subscribe converters
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/subconverter
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/jpcre2
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/rapidjson
+
+# Add smartdns
+svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
+
+# Add udptools
+git clone --depth=1 https://github.com/bao3/openwrt-udp2raw
+git clone --depth=1 https://github.com/bao3/openwrt-udpspeeder
+git clone --depth=1 https://github.com/bao3/luci-udptools
+
+# Add OpenAppFilter
+git clone --depth=1 https://github.com/destan19/OpenAppFilter
+popd
+
+# Fix libssh
+pushd feeds/packages/libs
+rm -rf libssh
+svn co https://github.com/openwrt/packages/trunk/libs/libssh
+popd

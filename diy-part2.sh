0a16086825d67a8036aa9d151aa9aa8b2981f2e0
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2020-09-22 18:23:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -43,10 +43,10 @@ sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_gene
 #mv luci-app-vssr/ package/
 #mv lua-maxminddb/ package/
 #jd
-git clone https://github.com/jerrykuku/node-request.git
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git  
-mv node-request/ package/
-mv luci-app-jd-dailybonus/ package/
+#git clone https://github.com/jerrykuku/node-request.git
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git  
+#mv node-request/ package/
+#mv luci-app-jd-dailybonus/ package/
 #luci-app-ipsec-vpnserver
 #rm -rf feeds/lienol/lienol/luci-app-ipsec-vpnserver-manyusers
 #git clone https://github.com/xiaoqingfengATGH/feeds-lienol

aef47dd7f1793a3d539f12ea4f9578e150550c71
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2021-02-23 11:19:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,16 +21,16 @@ mv lua-maxminddb/ package/
 
 
 # Add luci-app-ssr-plus
-pushd package/lean
-git clone --depth=1 https://github.com/fw876/helloworld
-popd
+#pushd package/lean
+#git clone --depth=1 https://github.com/fw876/helloworld
+#popd
 
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
 
 # Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+#git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 
 
 # Add luci-app-adguardhome

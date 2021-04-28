7aed06e6fe7d8612b951451a5e3b2c8c569e48ec
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2021-04-28 09:19:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,11 @@ git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr.git 
 mv luci-app-vssr/ package/
 mv lua-maxminddb/ package/
-
+#smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git 
+mv openwrt-smartdns/ package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git 
+mv luci-app-smartdns/ package/
 
 # Add luci-app-ssr-plus
 #pushd package/lean
